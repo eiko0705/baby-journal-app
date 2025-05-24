cb7769eb8120d2e63255c1d05daadd0721b7e8c2
eiko0705 <eiko.imai.070502@gmail.com>
2025-05-24 23:56:19 +0900

    feat: create extraReducer in slice

--- frontend/src/features/achievements/achievementsSlice.ts
+++ frontend/src/features/achievements/achievementsSlice.ts
@@ -1,28 +1,41 @@
-import { createSlice, nanoid } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import type { PayloadAction } from '@reduxjs/toolkit'
 import type { RootState } from '../../app/store'
 import type { Achievement, NewAchievementPayload } from '../../types'
+import { createAchievemntAPI, fetchAchievementsAPI } from '../../services/achievementService'
 
 interface AchievementsState {
     items: Achievement[]
+    status: 'idle' | 'loading' | 'succeeded' | 'failed'
+    error: string | null
 }
 
 const initialState: AchievementsState = {
-    items: []
+    items: [],
+    status: 'idle',
+    error: null,
 }
+
+export const fetchAchievements = createAsyncThunk<Achievement[]>(
+    'achievements/fetchAchievements',
+    async () => {
+        const response = await fetchAchievementsAPI();
+        return response
+    }
+)
+
+export const addNewAchievement = createAsyncThunk<Achievement, NewAchievementPayload>(
+    'achievements/addNewAchievemnt',
+    async (newAchievementData) => {
+        const response = await createAchievemntAPI(newAchievementData)
+        return response
+    }
+)
 
 export const achievementsSlice = createSlice({
     name: 'achievements',
     initialState,
     reducers: {
-        addAchievement: (state, action: PayloadAction<NewAchievementPayload>) => {
-            const newAchievement: Achievement = {
-                id: nanoid(),
-                ...action.payload,
-            }
-            state.items.push(newAchievement)
-            state.items.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime())
-        },
         removeAchievement: (state, action: PayloadAction<string>) => {
             state.items = state.items.filter(achievement => achievement.id !== action.payload)
         },
@@ -32,19 +45,38 @@ export const achievementsSlice = createSlice({
                 state.items[index] = action.payload
             }
         },
-        /*
-        setAchievementAge: (state, action: PayloadAction<Achievement>) => {
-            const achievements = state.items.find(item => item.id === action.payload.id)
-            if (achievements) {
-                achievements.ageAtEvent = action.payload.ageAtEvent
-            }
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchAchievements.pending, (state) => {
+                state.status = 'loading'
+            })
+            .addCase(fetchAchievements.fulfilled, (state, action: PayloadAction<Achievement[]>) => {
+                state.status = 'succeeded'
+                state.items = action.payload
+            })
+            .addCase(fetchAchievements.rejected, (state, action) => {
+                state.status = 'failed'
+                state.error = action.error.message || 'Failed to fetch achievements'
+            })
+            .addCase(addNewAchievement.pending, (state) => {
+                state.status = 'loading'
+            })
+            .addCase(addNewAchievement.fulfilled, (state, action: PayloadAction<Achievement>) => {
+                state.status = 'succeeded'
+                state.items.push(action.payload)
+            })
+            .addCase(addNewAchievement.rejected, (state, action) => {
+                state.status = 'failed'
+                state.error = action.error.message || 'Failed to add new achievements'
+            })
         }
-        */
-    }
 })
 
-export const { addAchievement, removeAchievement, updateAchievement } = achievementsSlice.actions
+export const { removeAchievement, updateAchievement } = achievementsSlice.actions
 export const selectAllAchievements = (state: RootState) => state.achievements.items
+export const selectAchievementsStatus = (state: RootState) => state.achievements.status
+export const selectAchievementsError = (state: RootState) => state.achievements.error
 export const selectAchievementById = (state: RootState, id: string) => state.achievements.items.find(item => item.id === id)
 
 export default achievementsSlice.reducer
